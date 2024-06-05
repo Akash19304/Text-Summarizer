534aca17f2980dbb136691436cf38a4bdf072831
Akash19304 <123761006+Akash19304@users.noreply.github.com>
2024-06-05 10:20:42 +0530

    fixed some bugs in the prediction pipeline

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
-import uvicorn 
-import sys 
-import os 
-from fastapi.templating import Jinja2Templates 
-from starlette.responses import RedirectResponse 
-from fastapi.responses import Response 
-from textSummarizer.pipeline.prediction import PredictionPipeline 
+import uvicorn
+import sys
+import os
+from fastapi.templating import Jinja2Templates
+from starlette.responses import RedirectResponse
+from fastapi.responses import Response
+from textSummarizer.pipeline.prediction import PredictionPipeline
 
 
-text: str = "What is Text Summarization?"
+text:str = "What is Text Summarization?"
 
 app = FastAPI()
 
@@ -22,23 +22,24 @@ async def index():
 async def training():
     try:
         os.system("python main.py")
-        return Response("Training Successful !!")
-    
+        return Response("Training successful !!")
+
     except Exception as e:
-        return Response(f"Error Occured ! {e}")
+        return Response(f"Error Occurred! {e}")
     
 
 
-@app.get("/predict") 
+
+@app.post("/predict")
 async def predict_route(text):
     try:
+
         obj = PredictionPipeline()
         text = obj.predict(text)
         return text
-    
     except Exception as e:
         raise e
     
 
-if __name__ == "__main__":
+if __name__=="__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
--- src/textSummarizer/pipeline/prediction.py
+++ src/textSummarizer/pipeline/prediction.py
@@ -8,17 +8,18 @@ class PredictionPipeline:
         self.config = ConfigrationManager().get_model_evaluation_config()
 
 
-    def predict(self, text):
+    
+    def predict(self,text):
         tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
-        gen_kwargs = {"length_penalty": 0.8, "num_beams": 8, "max_length": 128}
+        gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
 
-        pipe = pipeline("Summarization", model=self.config.model_path, tokenizer=tokenizer)
+        pipe = pipeline("summarization", model=self.config.model_path,tokenizer=tokenizer)
 
-        print("Dialogue: ")
+        print("Dialogue:")
         print(text)
 
         output = pipe(text, **gen_kwargs)[0]["summary_text"]
-        print("\nModel Sumamry: ")
+        print("\nModel Summary:")
         print(output)
 
         return output
